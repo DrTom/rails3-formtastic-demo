210b5f22a2ae1f640cfebed496d0fab9977c1f40
Thomas Schank <DrTom@schank.ch>
2011-03-08 19:05:32 +0100

    instial commit after project creation and gemset setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormtasticDemo::Application.config.secret_token = '0465117fea4eae3f3373cf695481ff93642707b2ec20963952b3994ff3ebf54e958219a352e6dd109c940548753000d0046b60279de73e9fedbfaa7f65a49229'
