100c23b3ad9ca827f35dc1a4b7f9bd911a78d77c
Thomas Schank <DrTom@schank.ch>
2011-03-09 07:50:43 +0100

    show gender in animal list

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,13 @@
 class Animal < ActiveRecord::Base
   belongs_to :category
+
+  def gender
+    # doesn't work ???
+    if female == true
+      "female"
+    else
+      "male"
+    end
+  end
+
 end
--- dbWorksheet.sql
+++ dbWorksheet.sql
@@ -2,3 +2,5 @@
 select * from schema_migrations;
 
 select * from categories;
+
+select * from animals;
